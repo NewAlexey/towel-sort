809b62a2a8ea37dbd5f8241971e2f4efd5add6df
NewAlexey <alexey.krupenia@gmail.com>
2021-02-18 00:17:59 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+const { strikethrough } = require("colors");
+
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined || matrix.length === 0) return [];
+    let ans = [];
+    matrix.forEach((item, index) => {
+        if (index % 2 === 0) {
+            ans.push(item);
+        } else {
+            ans.push(item.reverse());
+        }
+    });
+    ans = ans.reduce((acc, item) => {
+        item.forEach((element) => {
+            return [...acc, acc.push(element)];
+        });
+        return acc;
+    }, []);
+    return ans;
+};
